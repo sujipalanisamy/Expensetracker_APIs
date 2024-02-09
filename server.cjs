8d3062a518c0bb3aa2ec0fb0324444c0b17209a9
sujipalanisamy <sujipalanisami@gmail.com>
2024-02-09 11:11:45 +0530

    port change

--- server.cjs
+++ server.cjs
@@ -1,69 +1,71 @@
-const express=require('express')
-const bodyParser=require('body-parser')
+const express = require('express')
+const bodyParser = require('body-parser')
 const {ObjectId} = require('mongodb')
-const  {connectToDb,getDb}=require('./dbconnection.cjs')//these are xported from dbconnection.cjs and imported here using curly braces{connectiontodb,getdb}
+// Importing required functions from dbConnection.cjs
+const {connectToDb, getDb} = require('./dbConnection.cjs')
+
 const app = express()
 app.use(bodyParser.json())
-// app.get('/', function(request, response) {
-//     response.json({
-//         "status":"welcome"
-//     })
-// })
+
 let db
-connectToDb(function(error){
-    if(error){
+connectToDb(function(error) {
+    if(error) {
         console.log('Could not establish connection...')
         console.log(error)
-    }else{
-        app.listen(8000)
-        db=getDb()
-        console.log('getting to port 8000')
+    } else { // if no error in establishing connection
+        // process.env.PORT : cloud service
+        // 8000 : local machine
+        const port = process.env.PORT || 8000
+        app.listen(port)
+        db = getDb()
+        console.log(`Listening on port ${port}...`)
     }
 })
+
 /**
- * Express Tracker
- * Functionalities : adding entry,getting the summaries of previous entries,editing and deleting
- * Input file:Category,amount,date
+ * Expense Tracker
+ * Functionalities : adding entry, getting the summaries of previous entries, editing and deleting
+ * Input fields : Category, Amount, Date
  * 
- * CRUD: create,read,update and delete
+ * CRUD : Create, Read, Update and Delete
  * 
  * get-entries / get-data - GET
  * add-entry - POST
- * edit -entry - PATCH
+ * edit-entry - PATCH
  * delete-entry - DELETE
  */
-    
-app.post('/add-entry',function(request,response){
-    db.collection('ExpensesData').insertOne(request.body).then(function(){
+
+app.post('/add-entry', function(request, response) {
+    db.collection('ExpensesData').insertOne(request.body).then(function() {
         response.status(201).json({
-            "status":"Enter added successfully"
+            "status" : "Entry added successfully"
         })
-    }).catch(function(){
-        response.status(500).json({ 
-            "status":"Entry not added"
+    }).catch(function () {
+        response.status(500).json({
+            "status" : "Entry not added"
         })
     })
-
 })
 
 app.get('/get-entries', function(request, response) {
-    
-    const entries = []// Declaring an empty array
+    // Declaring an empty array
+    const entries = []
     db.collection('ExpensesData')
-    .find()//it will be pointing only one and so we are using for loop and storing it in an array
+    .find()
     .forEach(entry => entries.push(entry))
     .then(function() {
         response.status(200).json(entries)
     }).catch(function() {
-        response.status(404).json({      // 4 la start aagra status code client side error ah point pannu//5 means server side error
+        response.status(500).json({
             "status" : "Could not fetch documents"
         })
     })
 })
+
 app.delete('/delete-entry', function(request, response) {
-    if(ObjectId.isValid(request.query.id)) {//in the 3rd line above the object id is got required from the dbconnection.cjs
+    if(ObjectId.isValid(request.query.id)) {
         db.collection('ExpensesData').deleteOne({
-            _id : new ObjectId(request.query.id)//query.id this is given at the postman app after question mark as a query to select the id
+            _id : new ObjectId(request.query.id)
         }).then(function() {
             response.status(200).json({
                 "status" : "Entry successfully deleted"
@@ -79,25 +81,24 @@ app.delete('/delete-entry', function(request, response) {
         })
     }
 })
-//last request params or patch
-app.patch('/update-entry/:id',function(request,response){
-   if(ObjectId.isValid(request.params.id)){
-    db.collection('ExpensesData').updateOne(
-        {_id : new ObjectId(request.params.id)},
-        {$set : request.body}
-    ).then(function(){
-        response.status(200).json({
-            "status":"Entry updated successfully"
+
+app.patch('/update-entry/:id', function(request, response) {
+    if(ObjectId.isValid(request.params.id)) {
+        db.collection('ExpensesData').updateOne(
+            { _id : new ObjectId(request.params.id) }, // identifier : selecting the document which we are going to update
+            { $set : request.body } // The data to be updated
+        ).then(function() {
+            response.status(200).json({
+                "status" : "Entry updated successfully"
+            })
+        }).catch(function() {
+            response.status(500).json({
+                "status" : "Unsuccessful on updating the entry"
+            })
         })
-    }).catch(function(){
+    } else {
         response.status(500).json({
-            "status":"Unsuccessful updation of entry"
+            "status" : "ObjectId not valid"
         })
-    })
-   }else{
-    request.response(500).json({
-        "status":"Object Id is not valid"
-    })
-   }
-    
+    }
 })
